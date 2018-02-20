a56a37848d5bd29452c921fc76b23eadd072dc20
Viktors Ņikiforovs <viktors.nikiforovs@netcore.lv>
2018-02-20 13:00:17 +0200

    Panels as accordions.

--- Resources/assets/js/components/MapForm.vue
+++ Resources/assets/js/components/MapForm.vue
@@ -6,86 +6,107 @@
         <div class="panel-body">
             <div class="row">
                 <div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <span class="panel-title">Map settings</span>
-                        </div>
-                        <div class="panel-body">
-                            <div class="form-group">
-                                <label for="identifier">Map identifier:</label>
-                                <input type="text" id="identifier" class="form-control" v-model="map.identifier">
+                    <div class="panel-group" id="map-accordion">
+
+                        <!-- Settings collapse -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="settingsHeading">
+                                <a class="accordion-toggle collapsed" data-toggle="collapse"
+                                   data-parent="#map-accordion" href="#settingsCollapse">
+                                    <i class="fa fa-cogs"></i> &nbsp; Map settings
+                                </a>
                             </div>
+                            <div id="settingsCollapse" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <label for="identifier">Map identifier:</label>
+                                        <input type="text" id="identifier" class="form-control"
+                                               v-model="map.identifier">
+                                    </div>
 
-                            <div class="form-group">
-                                <label class="control-label">Map center:</label>
-                                <map-coordinates-row :marker="map"></map-coordinates-row>
-                            </div>
+                                    <div class="form-group">
+                                        <label class="control-label">Map center:</label>
+                                        <map-coordinates-row :marker="map"></map-coordinates-row>
+                                    </div>
 
-                            <div class="form-group m-b-0">
-                                <label for="map-zoom">Map zoom:</label>
-                                <input id="map-zoom" type="number" data-slider-min="1" data-slider-max="20"
-                                       data-slider-step="1" :data-slider-value="map.zoom">
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <span class="panel-title">Markers ({{ map.markers.length }})</span>
-                            <div class="panel-heading-btn">
-                                <button type="button" class="btn btn-xs btn-success" @click="addMarker()">
-                                    Add marker
-                                </button>
-                            </div>
-                        </div>
-                        <div class="panel-body">
-                            <map-coordinates-row
-                                    :marker="marker"
-                                    :key="marker.key"
-                                    :is-removable="true"
-                                    v-for="marker in map.markers"
-                                    @removeMarker="removeMarker(marker)">
-                            </map-coordinates-row>
-                        </div>
-                    </div>
-
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <span class="panel-title">Polygons ({{ map.polygons.length }})</span>
-                            <div class="panel-heading-btn">
-                                <button type="button" class="btn btn-xs btn-success" @click="addPolygon">
-                                    Add polygon
-                                </button>
-                            </div>
-                        </div>
-                        <div class="panel-body">
-
-                            <div class="panel panel-default" v-for="polygon in map.polygons" :key="polygon.key">
-                                <div class="panel-heading">
-                                    <div class="panel-heading-btn">
-                                        <button type="button" class="btn btn-xs btn-success"
-                                                @click="addMarker(polygon)">
-                                            Add marker
-                                        </button>
-
-                                        <button type="button" class="btn btn-xs btn-danger"
-                                                @click="removePolygon(polygon)">
-                                            Remove polygon
-                                        </button>
+                                    <div class="form-group m-b-0">
+                                        <label for="map-zoom">Map zoom:</label>
+                                        <input id="map-zoom" type="number" data-slider-min="1" data-slider-max="20"
+                                               data-slider-step="1" :data-slider-value="map.zoom">
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+
+                        <!-- Markers collapse -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="markersHeading">
+                                <a class="accordion-toggle collapsed" data-toggle="collapse"
+                                   data-parent="#map-accordion" href="#markersCollapse">
+                                    <i class="fa fa-map-marker"></i> &nbsp; Markers ({{ map.markers.length }})
+                                </a>
+                            </div>
+                            <div id="markersCollapse" class="panel-collapse collapse">
                                 <div class="panel-body">
+                                    <div class="button m-b-1 text-right">
+                                        <button type="button" class="btn btn-xs btn-success" @click="addMarker()">
+                                            <i class="fa fa-plus"></i> Add marker
+                                        </button>
+                                    </div>
+
                                     <map-coordinates-row
-                                            :key="marker.key"
                                             :marker="marker"
+                                            :key="marker.key"
                                             :is-removable="true"
-                                            v-for="marker in polygon.markers"
-                                            @removeMarker="removeMarker(marker, polygon)">
+                                            v-for="marker in map.markers"
+                                            @removeMarker="removeMarker(marker)">
                                     </map-coordinates-row>
                                 </div>
                             </div>
+                        </div>
 
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="polygonsHeading">
+                                <a class="accordion-toggle collapsed" data-toggle="collapse"
+                                   data-parent="#map-accordion" href="#polygonsCollapse">
+                                    <i class="fa fa-map-signs"></i> &nbsp; Polygons ({{ map.polygons.length }})
+                                </a>
+                            </div>
+                            <div id="polygonsCollapse" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <div class="m-b-1 text-right">
+                                        <button type="button" class="btn btn-xs btn-success" @click="addPolygon">
+                                            <i class="fa fa-plus"></i> Add polygon
+                                        </button>
+                                    </div>
+
+                                    <div class="panel panel-default" v-for="polygon in map.polygons" :key="polygon.key">
+                                        <div class="panel-heading">
+                                            <div class="panel-heading-btn">
+                                                <button type="button" class="btn btn-xs btn-success"
+                                                        @click="addMarker(polygon)">
+                                                    <i class="fa fa-plus"></i> Add marker
+                                                </button>
+
+                                                <button type="button" class="btn btn-xs btn-danger"
+                                                        @click="removePolygon(polygon)">
+                                                    <i class="fa fa-trash"></i> Remove polygon
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="panel-body">
+                                            <map-coordinates-row
+                                                    :key="marker.key"
+                                                    :marker="marker"
+                                                    :is-removable="true"
+                                                    v-for="marker in polygon.markers"
+                                                    @removeMarker="removeMarker(marker, polygon)">
+                                            </map-coordinates-row>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
